a507d4d3d30cf676ef41278b2d3dda8540c04680
isidoravs <isvizcaya@uc.cl>
2019-05-04 16:50:26 -0400

    user header

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -1,5 +1,6 @@
 import '../images/default-user.png';
 import '../images/logo-plane.png';
 import '../images/logo.png';
+import '../images/pin-icon.png';
 import '../images/search-icon.png';
 import '../images/user-icon.png';
