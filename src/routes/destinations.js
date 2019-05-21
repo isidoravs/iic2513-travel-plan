399d631b342cb69ff63482ecd07650a46d7bfbff
BenjaminFuentes <bgfuentes@uc.cl>
2019-05-21 16:09:52 -0400

    busqueda por dias, budget, destino y rating

--- src/routes/destinations.js
+++ src/routes/destinations.js
@@ -75,8 +75,47 @@ router.get('destinations.find', '/search', async (ctx) => {
 
 router.get('destinations.supersearch', '/ssearch', async (ctx) => {
   const simple = false;
+  const destination1 = ctx.request.query.destination1;
+  const destination2 = ctx.request.query.destination2;
+  const destination3 = ctx.request.query.destination3;
+  const rating = ctx.query.rating;
+  const min_b = ctx.query.minrangeb;
+  const max_b = ctx.query.maxrangeb;
+  const min_d = ctx.query.minranged;
+  const max_d = ctx.query.maxranged;
+  console.log(min_b, max_b)
+  const destinationSearch = await ctx.orm.destination.findAll({
+    where: {
+      destinationName: {
+        [op.like]: {
+          [op.any]: [`%${destination1}%`, `%${destination2}%`, `%${destination3}%`]
+        }
+      },
+    },
+  });
+  let itinerary_fin = [];
+  let itinerary_dest;
+  const itineraries = await Promise.all(destinationSearch.map(destination => destination.getItineraries()));
+  itineraries.forEach((destination) => {
+    itinerary_dest = [];
+    destination.forEach((itinerary) => {
+      if (itinerary.avgScore >= rating){
+        if (itinerary.budget <= max_b && itinerary.budget >= min_b){
+          let days = (Date.parse(itinerary.endDate) - Date.parse(itinerary.startDate))/86400000;
+          if (days <= max_d && days >= min_d){
+            itinerary_dest.push(itinerary);
+          }
+        }
+      }
+    })
+    itinerary_fin.push(itinerary_dest);
+  })
   await ctx.render('/search', {
     simple,
+    destinationSearch,
+    itinerary_fin,
+    showItineraryPath: itinerary => ctx.router.url('itineraries.show', { id: itinerary.id }),
+    showDestinationPath: destination => ctx.router.url('destinations.show', { id: destination.id }),
     superSearchPath: ctx.router.url('destinations.supersearch'),
   })
   // const rating = ctx.request.query.value;
