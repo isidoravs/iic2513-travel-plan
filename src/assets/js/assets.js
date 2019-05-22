9a32c5f1129c7c620ccd709a43736a79663f2aa3
isidoravs <isvizcaya@uc.cl>
2019-05-21 22:20:45 -0400

    home image

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -1,6 +1,7 @@
 import '../images/default-user.png';
 import '../images/default-itinerary.png';
 import '../images/edit-icon.png';
+import '../images/home-background.jpg';
 import '../images/logo-plane.png';
 import '../images/logo.png';
 import '../images/pin-icon.png';
