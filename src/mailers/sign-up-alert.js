3b413e1fc3e2d8f9c904ef093904849a7cd773a1
isidoravs <isvizcaya@uc.cl>
2019-05-22 12:25:42 -0400

    minor fix template mail

--- src/mailers/sign-up-alert.js
+++ src/mailers/sign-up-alert.js
@@ -1,3 +1,3 @@
 module.exports = function sendSignUpAlertEmail(ctx, { user }) {
-  return ctx.sendMail('sign-up-alert', { to: user.email, subject: 'Alert!' }, { user });
+  return ctx.sendMail('sign-up-alert', { to: user.email, subject: 'Bienvenido a Travel&Plan' }, { user });
 };
