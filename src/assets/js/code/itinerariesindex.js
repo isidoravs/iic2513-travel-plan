af1caf846ecfb0ee10accdb7aa5168b37fbae950
BenjaminFuentes <bgfuentes@uc.cl>
2019-06-03 01:50:17 -0400

    filter by name

--- src/assets/js/code/itinerariesindex.js
+++ src/assets/js/code/itinerariesindex.js
@@ -1,11 +1,8 @@
 $(function(){
   $('#namefilter').on("keyup",function(){
   let input = $(this).val().toLowerCase();
-  let c = 0;
   $("#it-list div").filter(function(){
-    c++;
     $(this).toggle($(this).attr('id').toLowerCase().indexOf(input) > -1)
   })
-  $('#ejemplo').text(c);
   });
 });
